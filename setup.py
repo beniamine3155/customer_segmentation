06efdecdb79bdd5445ed97f34e8ea67adf8546cb
beniamine3155 <beniamine3155@gmail.com>
2025-11-03 20:32:35 +0600

    define project folder structure

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="beniamine nahid",
+    author_email="beniamine3155@gmail.com",
+    packages=find_packages()
+)
