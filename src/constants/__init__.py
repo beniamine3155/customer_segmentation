c306700cead7a747e7aa58531da0f513e7bed3b6
beniamine3155 <beniamine3155@gmail.com>
2025-11-04 06:16:16 +0600

    implemented logger, exception and mongodb connection

---
+++ src/constants/__init__.py
@@ -0,0 +1,21 @@
+import os
+from datetime import date
+
+# For MongoDB connection
+DATABASE_NAME = "CustomerSegmentationDB"
+COLLECTION_NAME = "CustomerData"
+MONGODB_URL_KEY = "MONGODB_URL"
+
+PIPELINE_NAME: str = ""
+ARTIFACT_DIR: str = "artifact"
+
+MODEL_FILE_NAME = "model.pkl"
+
+TARGET_COLUMN = "Response"
+CURRENT_YEAR = date.today().year
+PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+
+FILE_NAME: str = "data.csv"
+TRAIN_FILE_NAME: str = "train.csv"
+TEST_FILE_NAME: str = "test.csv"
+SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
