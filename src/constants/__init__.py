0bb3124ecdb28b75ba45ed65d65bc3a26456033c
beniamine3155 <beniamine3155@gmail.com>
2025-11-04 06:34:52 +0600

    setup artifact, config, constants for data ingestion

--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -13,9 +13,18 @@ MODEL_FILE_NAME = "model.pkl"
 
 TARGET_COLUMN = "Response"
 CURRENT_YEAR = date.today().year
-PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
+PREPROCSSING_OBJECT_FILE_NAME = "scaler.pkl"
 
 FILE_NAME: str = "data.csv"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
+
+
+"""
+Data Ingestion related constant start with DATA_INGESTION VAR NAME
+"""
+DATA_INGESTION_COLLECTION_NAME: str = "CustomerData"
+DATA_INGESTION_DIR_NAME: str = "data_ingestion"
+DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
+DATA_INGESTION_INGESTED_DIR: str = "ingested"
